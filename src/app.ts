3df0970dc9b4813d2f5e103b7933dfd9d75c3a78
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-05-14 16:15:26 -0300

    Conectando no MongoDB Atlas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
   public app: express.Application;
@@ -7,6 +9,18 @@ class App {
   public constructor() {
     this.app = express();
     this.app.use(cors());
+
+    this.initMongoose();
+    this.connectDatabase();
+  }
+
+  private initMongoose(): void {
+    mongoose.set('runValidators', true);
+  }
+
+  private connectDatabase(): void {
+    mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@${process.env.MONGO_DB_CLUSTER}/crud-nodejs?retryWrites=true&w=majority`);
   }
 
   public listen(port: number): void {
